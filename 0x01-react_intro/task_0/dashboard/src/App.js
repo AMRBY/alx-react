45d70baca65bbee28ef448bf1bbc840a5a017a43
AMRBY <bouyoussefamr@gmail.com>
2024-10-31 23:14:35 +0000

    mandatory

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function App() {
+  return (
+    <body>
+     <header>
+      <div className="App-header">
+	<h1>School dashboard</h1>
+      </div>
+     </header>
+     <hr>
+     <div className="App-body">
+	<p>Login to access the full dashboard</p>
+     </div>
+     <hr>
+     <footer>
+      <div className="App-footer">
+	<p>Copyright 2020 - holberton School</p>
+      </div>
+     </footer>
+    </body>
+  );
+}
+
+export default App;
