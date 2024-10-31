2864e92c1d986e0811cabe816b1ae636534f8eaa
AMRBY <bouyoussefamr@gmail.com>
2024-10-31 23:38:07 +0000

    mandatory

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -5,14 +5,15 @@ function App() {
     <body>
      <header>
       <div className="App-header">
+	<img src="./holberton-logo.jpg" alt="" width="100" height="100"></img>
 	<h1>School dashboard</h1>
       </div>
      </header>
-     <hr>
+     <hr/>
      <div className="App-body">
 	<p>Login to access the full dashboard</p>
      </div>
-     <hr>
+     <hr/>
      <footer>
       <div className="App-footer">
 	<p>Copyright 2020 - holberton School</p>
